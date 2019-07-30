085df9ac8b78a19e4e001b47bc339e6d57a6ca3a
Faiz <faiz96dev@gmail.com>
2019-07-30 12:27:44 +0600

    change

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
-menu.onclick = function myFunction() {
-    var x = document.getElementById('myTopnav');
+// menu.onclick = function myFunction() {
+//     var x = document.getElementById('myTopnav');
 
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else{
-        x.className = "topnav";
-    }
-}
+//     if (x.className === "topnav") {
+//         x.className += " responsive";
+//     } else{
+//         x.className = "topnav";
+//     }
+// }
+
+// $('ul.nav').find('a').click(function(){
+//     var $href = $(this).attr('href');
+//     var $anchor = $('#'+$href).offset();
+//     window.scrollTo($anchor.left,$anchor.top);
+//     return false;
+// });
+
+console.log('Hui')
+
+// $(function(){
+
+//     $('.some_link').on('click', function(e){
+//       $('html,body').stop().animate({ scrollTop: $('#some_point').offset().top }, 1000);
+//       e.preventDefault();
+//     });
+    
+//     });
+
+$(document).ready(function(){
+    $('a[href*=#]').bind("click", function(e){
+    var anchor = $(this);
+    $('html, body').stop().animate({
+    scrollTop: $(anchor.attr('href')).offset().top
+    }, 1000);
+    e.preventDefault();
+    });
+    return false;
+    });
